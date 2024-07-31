4687747fc201fd89e73ac2d9255514438ae7f879
Divyansh Sharma <shan17div@gmail.com>
2024-07-31 22:49:47 +0530

    Basic Ui Setup

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main()
   .then((res) => {
@@ -22,6 +23,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+  res.send("Systummmmm");
+});
 
 app.get("/listings", async (req, res) => {
   let allListings = await Listing.find();
