c6302949f30fc03b7437e8b65d4f265d7dfd234e
Divyansh Sharma <shan17div@gmail.com>
2024-08-12 14:05:51 +0530

    Navbar ui set

---
+++ public/js/toggle.js
@@ -0,0 +1,16 @@
+let userNavbtn = document.querySelector(".registerbox");
+let userDataBox = document.querySelector(".userdatabox");
+userNavbtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  userDataBox.classList.toggle("hide");
+  if (!userDataBox.classList.contains("hide")) {
+    window.addEventListener("click", hideUserdataBox);
+  }
+});
+
+// to add the navbar login or signup button if clicked on userNavbtn and to remove that if user click anywhere on screen
+
+function hideUserdataBox() {
+  userDataBox.classList.add("hide");
+  window.removeEventListener("click", hideUserdataBox);
+}
