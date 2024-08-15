4e32547b1b489092741edca718f22840d143552e
Divyansh Sharma <shan17div@gmail.com>
2024-08-15 17:33:55 +0530

    Home page responsive

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,18 +1,35 @@
-// For toggling a navbar effect
-// to add the navbar login or signup button if clicked on userNavbtn and to remove that if user click anywhere on screen
+// For toggling a navbar effect on desktop
 let userNavbtn = document.querySelector(".registerbox");
 let userDataBox = document.querySelector(".userdatabox");
+
 userNavbtn.addEventListener("click", (event) => {
   event.stopPropagation();
   userDataBox.classList.toggle("hide");
   if (!userDataBox.classList.contains("hide")) {
-    window.addEventListener("click", hideUserdataBox);
+    window.addEventListener("click", hideUserdataBoxDesktop);
   }
 });
 
-function hideUserdataBox() {
+function hideUserdataBoxDesktop() {
   userDataBox.classList.add("hide");
-  window.removeEventListener("click", hideUserdataBox);
+  window.removeEventListener("click", hideUserdataBoxDesktop);
+}
+
+// For toggling a navbar effect on mobile
+let userNavbtnmob = document.querySelector(".rgstr-mob");
+let userDataBoxmob = document.querySelector(".userdatabox-mob");
+
+userNavbtnmob.addEventListener("click", (event) => {
+  event.stopPropagation();
+  userDataBoxmob.classList.toggle("hide");
+  if (!userDataBoxmob.classList.contains("hide")) {
+    window.addEventListener("click", hideUserdataBoxMobile);
+  }
+});
+
+function hideUserdataBoxMobile() {
+  userDataBoxmob.classList.add("hide");
+  window.removeEventListener("click", hideUserdataBoxMobile);
 }
 
 // For adding taxes functionality
